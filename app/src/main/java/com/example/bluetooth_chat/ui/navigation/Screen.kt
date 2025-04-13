6ec4517518463d3c9e9e3501121ed6fed03a3ad1
apavlovic2 <pavlovicantonio12@gmail.com>
2025-04-13 03:37:05 +0200

    added bluetooth logic to the bluetooth devices screen so it can scan for available devices

--- app/src/main/java/com/example/bluetooth_chat/ui/navigation/Screen.kt
+++ app/src/main/java/com/example/bluetooth_chat/ui/navigation/Screen.kt
@@ -1,8 +1,8 @@
 package com.example.bluetooth_chat.ui.navigation
 
 sealed class Screen(val route: String) {
-    object Home : Screen("home")
-    object Chat : Screen("chat")
-    object Groups : Screen("groups")
-    object BluetoothDevices : Screen("bluetooth_devices")
+    data object Home : Screen("home")
+    data object Chat : Screen("chat")
+    data object Groups : Screen("groups")
+    data object BluetoothDevices : Screen("bluetooth_devices")
 }
