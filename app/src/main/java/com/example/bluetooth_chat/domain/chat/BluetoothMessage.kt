250977a5a315c8355887729ea653a376d22a8eb7
jurapura13 <ju3a@protonmail.com>
2025-05-31 18:51:25 +0200

    Fixed the BluetoothMessage.kt structure

--- app/src/main/java/com/example/bluetooth_chat/domain/chat/BluetoothMessage.kt
+++ app/src/main/java/com/example/bluetooth_chat/domain/chat/BluetoothMessage.kt
@@ -3,5 +3,9 @@ package com.plcoding.bluetoothchat.domain.chat
 data class BluetoothMessage(
     val message: String,
     val senderName: String,
-    val isFromLocalUser: Boolean
+    val isFromLocalUser: Boolean,
+    val isFile: Boolean = false,
+    val fileName: String? = null,
+    val fileSize: Long? = null
 )
+
